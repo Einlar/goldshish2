a549b11a193c02a66794cec6a9fd058844d70813
Francesco Manzali <manzali.francesco97@gmail.com>
2023-09-03 14:50:04 +0200

    Add Spinner loader to CanvasLayer

--- frontend/src/lib/components/pdf/utils/CanvasLayer.svelte
+++ frontend/src/lib/components/pdf/utils/CanvasLayer.svelte
@@ -2,17 +2,28 @@
 	import type { PDFPageProxy, RenderTask } from 'pdfjs-dist/types/src/display/api';
 	import type { PageViewport } from 'pdfjs-dist/types/src/display/display_utils';
 	import type { Action } from 'svelte/action';
+	import Spinner from './Spinner.svelte';
+	import { createEventDispatcher } from 'svelte';
 
 	export let viewport: PageViewport;
 	export let page: PDFPageProxy;
 
+	const dispatch = createEventDispatcher();
 	let renderTask: RenderTask | undefined;
+	let loading = false;
 
 	const renderPage: Action<HTMLCanvasElement> = (canvas) => {
+		loading = true;
+
 		// Render the page
 		renderTask = page.render({
 			canvasContext: canvas.getContext('2d')!,
 			viewport
+		});
+
+		renderTask.promise.then(() => {
+			loading = false;
+			dispatch('rendered');
 		});
 
 		return {
@@ -23,10 +34,24 @@
 	};
 </script>
 
-<canvas width={viewport.width} height={viewport.height} use:renderPage />
+<div class="viewport" style="width: {viewport.width}px; height: {viewport.height}px;">
+	<canvas width={viewport.width} height={viewport.height} use:renderPage />
+	{#if loading}
+		<div class="loader">
+			<Spinner />
+		</div>
+	{/if}
+</div>
 
 <style>
-	canvas {
-		border: 2px solid blue;
+	.viewport {
+		position: relative;
+	}
+
+	.loader {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
